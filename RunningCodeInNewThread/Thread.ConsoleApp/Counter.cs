98130269c44389321f05060a3b3e4fb89f4f0fc8
Line-B-333 <10187764@ucn.dk>
2025-10-23 16:54:41 +0200

    Added: Folder to folder

---
+++ RunningCodeInNewThread/Thread.ConsoleApp/Counter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading; // Husk denne import for Thread.Sleep
+
+namespace ThreadsExercise.ConsoleApp;
+
+public abstract class Counter
+{
+
+    //Method that counts from 1 to 10 using a while loop
+    public static void CountToTen()
+    {
+        int index = 1;
+        while (index <= 10)
+        {
+            Console.WriteLine(index);
+            Thread.Sleep(2000);
+            index++;
+        }
+    }
+    
+    //Object: 
+    public static void CountToTenNamed(object threadName)
+    {
+        string name = (string)threadName; // Cast fra object til string
+        int index2 = 1;
+        while (index2 <= 10)
+        {
+            Console.WriteLine($"Thread {threadName}" + index2);
+            Thread.Sleep(2000);
+            index2++;
+        }
+    }
+
+
+    /* Assigment 3 part 1
+    public static void CountToTenNamed(string threadName) 
+    {
+        int index2 = 1;
+        while (index2 <= 10)
+        {
+            Console.WriteLine($"Thread {threadName}" + index2);
+            Thread.Sleep(2000);
+            index2++;
+        }
+    }*/
+}
---
+++ RunningCodeInNewThread/Thread.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ThreadsExercise.ConsoleApp;
+
+internal class Program
+{
+    
+    static void Main(string[] args)
+    {
+
+        // Opret tre tråde som skal køre CountToTenNamed
+        Thread t1 = new Thread(new ParameterizedThreadStart(Counter.CountToTenNamed));
+        Thread t2 = new Thread(new ParameterizedThreadStart(Counter.CountToTenNamed));
+        Thread t3 = new Thread(new ParameterizedThreadStart(Counter.CountToTenNamed));
+
+        // Start dem med forskellige navne
+        t1.Start("First thread");
+        t2.Start("Second thread");
+        t3.Start("Third thread");
+
+
+        // Assignment 3 part 1, her sker der en fejl,
+        // fordi ParameterizedThreadStart forventer en metode med et parameter typen object
+        //Thread thread = new Thread(new ParameterizedThreadStart(Counter.CountToTenNamed));
+
+        /* Assignment 2
+        Console.WriteLine("Count 1 to 10");
+
+        // Opret en ny Thread og giv CountToTen som metode
+        Thread countThread = new Thread(Counter.CountToTen);
+
+        // Start tråden
+        countThread.Start();
+
+        // (valgfrit) Hvis du vil vente på, at tråden bliver færdig, kan du gøre dette:
+        // countThread.Join();
+
+        Console.WriteLine("Counting started on a separate thread!");
+       */
+
+    }
+
+
+}
